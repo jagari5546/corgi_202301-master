e92cade6c7090207ffe8268cc63284e93e3d7164
jagari5546 <jagari5546@gmail.com>
2023-10-10 23:58:32 -0500

    Cambios en coin.cs
    aaaaa

--- Assets/Juego/Scripts/Coin.cs
+++ Assets/Juego/Scripts/Coin.cs
@@ -32,43 +32,7 @@ namespace MoreMountains.CorgiEngine
 		}
         
     }
-	public class Contador : GameManager
-		
-    {
-		GameManager gm;
-		public int Puntos;
-		public int Division;
-
-		[MMInformation("Add this component to an object with a Collider2D set as trigger, and it'll become pickable by Player Characters. When picked, it'll increase the amount of lives as specified. You can decide here to have only new lives added, within the limit of current lives containers, expand this limit, fill it accordingly, or fill all containers.", MMInformationAttribute.InformationType.Info, false)]
-		[Header("Normal one ups")]
-
-		[Tooltip("the amount of lives that should be added when picking this item")]
-		public int NumberOfAddedLives;
-		[Header("Containers")]
-		[Tooltip("the number of empty containers to add when picking this item")]
-		public int NumberOfAddedEmptyContainers;
-		[Tooltip("whether to fill the additional containers or not")]
-		public bool FillAddedContainers = false;
-		[Tooltip("whether to fill all containers or not")]
-		public bool FillAllContainers = false;
-
-
-		private void Update()
-        {
-			gm = FindObjectOfType<GameManager>();
-			Puntos = Points;
-			Division = Puntos % 10;
-			Debug.Log(Puntos);
-            if (Division==0)
-            {
-				GameManager.Instance.GainLives(NumberOfAddedLives);
-				GameManager.Instance.AddLives(NumberOfAddedEmptyContainers, FillAddedContainers);
-				if (FillAllContainers)
-				{
-					GameManager.Instance.GainLives(GameManager.Instance.MaximumLives);
-				}
-			}
-		}
-    }
+	
+    
 
 }

1dc74bc11375e6b26a520566837be71c41089436
jagari5546 <jagari5546@gmail.com>
2023-11-14 06:05:08 -0500

    Jefe v1

---
+++ Assets/Juego/Assets/Jefe/Jefe.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using MoreMountains.CorgiEngine;
+using MoreMountains.Tools;
+
+public class Jefe : MonoBehaviour
+{
+    private Animator animator;
+
+    public Rigidbody2D rb2D;
+
+    public Transform jugador;
+
+    private bool mirandoDerecha = true;
+
+    [Header("Vida")]
+
+    [SerializeField] private float vida;
+
+    [SerializeField] private BarraDeVida barraDeVida;
+
+    [Header("Ataque")]
+    [SerializeField] private Transform controladorAtaque;
+    [SerializeField] private float radioAtaque;
+    [SerializeField] private float dañoAtaque;
+
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb2D = GetComponent<Rigidbody2D>();
+        barraDeVida.InicializarBarraDeVida(vida);
+        jugador = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        float distanciaJugador = Vector2.Distance(transform.position, jugador.position);
+        animator.SetFloat("distanciaJugador", distanciaJugador);
+    }
+
+    public void TomarDaño(float daño)
+    {
+        vida -= daño;
+
+        barraDeVida.CambiarVidaActual(vida);
+
+        if (vida <= 0)
+        {
+            animator.SetTrigger("Muerte");
+        }
+    }
+
+    private void Muerte()
+    {
+        Destroy(gameObject);
+    }
+
+    public void MirarJugador()
+    {
+        if((jugador.position.x > transform.position.x && !mirandoDerecha) || (jugador.position.x < transform.position.x && mirandoDerecha))
+        {
+            mirandoDerecha = !mirandoDerecha;
+            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y + 180, 0);
+
+        }
+    }
+
+    //public void Ataque()
+    //{
+       // Collider2D[] objetos = Physics2D.OverlapCircleAll(controladorAtaque.position, radioAtaque);
+
+       // foreach (Collider2D colision in objetos)
+       // {
+         //   if (colision.CompareTag("Player"))
+         //   {
+                //colision.GetComponent<CombateJugaador>().TomarDaño(dañoAtaque));
+          //  }
+      //  }
+  //  }
+
+   // private void OnDrawGizmos()
+   // {
+   //     Gizmos.color = Color.red;
+   //     Gizmos.DrawWireSphere(controladorAtaque.position, radioAtaque);
+   // }
+
+
+
+}
